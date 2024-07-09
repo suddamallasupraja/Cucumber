ebf56c21de30a10f8af41846717ae8a3cd4ee8c5
suddamallasupraja <131091986+suddamallasupraja@users.noreply.github.com>
2024-07-09 21:04:20 +0530

    Update TestRunner.java

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -6,13 +6,13 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(//features= {".//Features//prac.feature"},
+@CucumberOptions(features= {".//Features//prac.feature"},
 				glue="stepDefinitions",
 				//features="@target/rerun.txt",
 				plugin= {"pretty","html:reports/myreport.html","json:reports/myreport.json","rerun:target/rerun.txt"},
 				monochrome=true, //to remove junk data in console window
-				dryRun=false,//to execute the functionality in browser otherwise jus it will check the feature file have corresponding method or not
-				tags="@without"
+				dryRun=false//to execute the functionality in browser otherwise jus it will check the feature file have corresponding method or not
+				//tags="@without"
 				
 				
 		)
